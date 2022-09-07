439d1813fb1dd7fd30467b8d5a55d798eca61edc
erin petra sofiya moon <erin@hecke.rs>
2022-09-07 17:12:40 -0500

    fifo tweaks

--- hw/fifo.sv
+++ hw/fifo.sv
@@ -1,29 +1,28 @@
+`default_nettype none
 // fifo.sv
 // Implements delay buffer (fifo)
 // On reset all entries are set to 0
 // Shift causes fifo to shift out oldest entry to q, shift in d
 
 module fifo #(
-  parameter DEPTH=8,
-  parameter BITS=64
+  parameter int DEPTH=8,
+  parameter int BITS=64
 ) (
-  input clk,rst_n,en,
-  input [BITS-1:0] d,
-  output [BITS-1:0] q
+  input wire clk,rst_n,en,
+  input wire [BITS-1:0] d,
+  output wire [BITS-1:0] q
 );
 
-  logic [BITS-1:0] registers [0:DEPTH-1];
+  logic [BITS-1:0] registers [DEPTH-1:0];
 
   always_ff @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-      for (int i=0; i<DEPTH; i++)
+    for (integer i = 0; i < DEPTH; i++) begin
+      if (!rst_n)
         registers[i] <= '0;
-    end else begin
-      registers[0] <= d;
-      for (int i=1; i<DEPTH; i++)
-        registers[i] <= registers[i-1];
+      else
+        registers[i] <= (i == 0) ? d : registers[i-1];
     end
   end
 
-  wire q = registers[DEPTH];
+  assign q = registers[DEPTH-1];
 endmodule // fifo
