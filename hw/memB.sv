271c6210ca4f388a36e5c6cb8d76251d7a10c3bd
erin petra sofiya moon <erin@hecke.rs>
2022-09-22 17:04:23 -0500

    memB: first-pass implementation

--- hw/memB.sv
+++ hw/memB.sv
@@ -7,4 +7,19 @@ module memB #(
   input  logic signed [BITS_AB-1:0] Bin [DIM-1:0],
   output logic signed [BITS_AB-1:0] Bout [DIM-1:0]
 );
+  genvar col_idx;
+  generate
+    for (col_idx = 0; col_idx < DIM; col_idx++) begin : col_fifos
+      fifo #(
+        .DEPTH(DIM+col_idx),
+        .BITS (BITS_AB)
+      ) col_fifo (
+        .clk  (clk),
+        .rst_n(rst_n),
+        .en   (en),
+        .d    (Bin[col_idx]),
+        .q    (Bout[col_idx])
+      );
+    end
+  endgenerate
 endmodule // memB
